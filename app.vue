847ce28359d696297926f6500f1baf93e1f3cbde
IagoFar <neila.galiza@hotmail.com>
2025-05-20 20:01:40 +0200

    Added a page rebuilder on url change

--- app.vue
+++ app.vue
@@ -4,8 +4,11 @@
       <NuxtRouteAnnouncer />
     </OrientationBlocker>
   </div>
-  <NuxtPage />
+  <NuxtPage :key="$route.fullPath" />
 </template>
 <script setup lang="ts">
 import OrientationBlocker from "~/OrientationBlocker.vue";
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
 </script>
